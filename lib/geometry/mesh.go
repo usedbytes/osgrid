007c52d83b6ea7b7071cc171641d1d91894abfa3
Brian Starkey <stark3y@gmail.com>
2022-06-24 21:23:20 +0100

    lib/geometry: Mesh creation

---
+++ lib/geometry/mesh.go
@@ -0,0 +1,153 @@
+package geometry
+
+import (
+	"math"
+
+	"github.com/usedbytes/osgrid/lib/surface"
+)
+
+type Mesh struct {
+	Vertices  [][3]float64
+	Triangles [][3]uint
+	WindingCCW bool
+	HScale float64
+	VScale float64
+}
+
+// Description of a mesh in terms of indices, to create triangles
+type indexLayout struct {
+	// The starting indices for the bottom/top of the first strip
+	bStart, tStart int
+	// Step between adjacent vertices in a strip
+	step int
+	// Stride between strips
+	stride int
+
+	// Number of columns and rows of vertices
+	cols, rows int
+}
+
+func makeTriangles(l *indexLayout, ccw bool) [][3]uint {
+	strips := l.rows - 1
+
+	// There's (cols * 2) - 2 triangles per strip
+	tris := make([][3]uint, 0, ((2*l.cols - 2) * strips))
+
+	for strip := 0; strip < int(strips); strip++ {
+		bIdx := l.bStart + strip * l.stride
+		tIdx := l.tStart + strip * l.stride
+
+		for col := 0; col < l.cols - 1; col++ {
+			v0, v1, v2, v3 := uint(bIdx), uint(bIdx+l.step), uint(tIdx), uint(tIdx+l.step)
+
+			if ccw {
+				tris = append(tris, [3]uint{v0, v1, v3})
+				tris = append(tris, [3]uint{v3, v2, v0})
+			} else {
+				tris = append(tris, [3]uint{v0, v2, v3})
+				tris = append(tris, [3]uint{v3, v1, v0})
+			}
+
+			bIdx += l.step
+			tIdx += l.step
+		}
+	}
+
+	return tris
+}
+
+// Options:
+// HScale
+// VScale --> No? Should apply to surface first.
+// WallThickness
+// Winding
+// 
+
+type GenerateOpt func(*Mesh)
+
+func GenerateMesh(s *surface.Surface, opts ...GenerateOpt) Mesh {
+	m := Mesh{
+		HScale: 1.0,
+		VScale: 1.0,
+	}
+
+	for _, opt := range opts {
+		opt(&m)
+	}
+
+	rows := len(s.Data)
+	cols := len(s.Data[0])
+	hstep := float64(s.Resolution) * m.HScale
+
+	// Top and bottom
+	m.Vertices = make([][3]float64, rows * cols * 2);
+
+	for r := 0; r < len(s.Data); r++ {
+		y := float64(r) * hstep
+		// Surface is top-left to bottom right, but we want bottom-left to top-right
+		// TODO: Should be an option or something
+		row := s.Data[len(s.Data)-r-1]
+
+		for c, v := range row {
+			x := float64(c) * hstep
+
+			topIdx := r * cols + c
+			baseIdx := cols * rows + topIdx
+
+			m.Vertices[topIdx] = [3]float64{ x, y, v * m.VScale }
+			m.Vertices[baseIdx] = [3]float64{ x, y, math.Min(s.Min, 0) }
+		}
+	}
+
+	layout := indexLayout{
+		bStart: 0,
+		tStart: cols,
+		step: 1,
+		stride: cols,
+
+		rows: rows,
+		cols: cols,
+	}
+	topTris := makeTriangles(&layout, m.WindingCCW)
+	m.Triangles = topTris
+
+	// Base triangles
+	// Winding is reversed so that the base faces outwards
+	layout.bStart = rows * cols
+	layout.tStart = rows * cols + layout.stride
+	baseTris := makeTriangles(&layout, !m.WindingCCW)
+	m.Triangles = append(m.Triangles, baseTris...)
+
+	// South face
+	layout.bStart = rows * cols
+	layout.tStart = 0
+	layout.rows = 2
+	southTris := makeTriangles(&layout, m.WindingCCW)
+	m.Triangles = append(m.Triangles, southTris...)
+
+	// North face
+	layout.bStart = (rows * cols * 2) - 1
+	layout.tStart = (rows * cols) - 1
+	layout.step = -1
+	layout.rows = 2
+	northTris := makeTriangles(&layout, m.WindingCCW)
+	m.Triangles = append(m.Triangles, northTris...)
+
+	// East face
+	layout.bStart = (rows * cols) + cols - 1
+	layout.tStart = cols - 1
+	layout.step = cols
+	layout.rows = 2
+	eastTris := makeTriangles(&layout, m.WindingCCW)
+	m.Triangles = append(m.Triangles, eastTris...)
+
+	// West face
+	layout.bStart = (rows * cols * 2) - cols
+	layout.tStart = (rows * cols) - cols
+	layout.step = -cols
+	layout.rows = 2
+	westTris := makeTriangles(&layout, m.WindingCCW)
+	m.Triangles = append(m.Triangles, westTris...)
+
+	return m
+}
---
+++ lib/geometry/mesh_test.go
@@ -0,0 +1,148 @@
+package geometry
+
+import (
+	"testing"
+
+	"github.com/usedbytes/osgrid/lib/surface"
+)
+
+func TestMakeTriangles(t *testing.T) {
+	// Layout for a simple 4-vert quad
+	l := indexLayout{
+		bStart: 0,
+		tStart: 2,
+		step: 1,
+		stride: 2,
+		cols: 2,
+		rows: 2,
+	}
+
+	tris := makeTriangles(&l, true)
+
+	exp := [][3]uint{
+		{ 0, 1, 3 },
+		{ 3, 2, 0 },
+	}
+
+	if len(tris) != len(exp) {
+		t.Errorf("incorrect number of triangles. Expected %v, got %v", len(exp), len(tris))
+	}
+
+	for i, tri := range tris {
+		if exp[i][0] != tri[0] || exp[i][1] != tri[1] || exp[i][2] != tri[2] {
+			t.Errorf("incorrect triangle %v. Expected %v, got %v", i, exp[i], tri)
+		}
+	}
+}
+
+func TestMakeTrianglesCW(t *testing.T) {
+	// Layout for a simple 4-vert quad
+	l := indexLayout{
+		bStart: 0,
+		tStart: 2,
+		step: 1,
+		stride: 2,
+		cols: 2,
+		rows: 2,
+	}
+
+	tris := makeTriangles(&l, false)
+
+	exp := [][3]uint{
+		{ 0, 2, 3 },
+		{ 3, 1, 0 },
+	}
+
+	if len(tris) != len(exp) {
+		t.Errorf("incorrect number of triangles. Expected %v, got %v", len(exp), len(tris))
+	}
+
+	for i, tri := range tris {
+		if exp[i][0] != tri[0] || exp[i][1] != tri[1] || exp[i][2] != tri[2] {
+			t.Errorf("incorrect triangle %v. Expected %v, got %v", i, exp[i], tri)
+		}
+	}
+}
+
+func TestGenerateSimple(t *testing.T) {
+	surf := surface.Surface{
+		Data: [][]float64{
+			{ 1, 2 },
+			{ 3, 4 },
+		},
+		Min: 1,
+		Max: 4,
+		Resolution: 1,
+	}
+
+	m := GenerateMesh(&surf)
+
+	if len(m.Triangles) != (2 * 6) {
+		t.Errorf("incorrect number of triangles. Expected %v, got %v", 2 * 6, len(m.Triangles))
+	}
+
+	if len(m.Vertices) != 8 {
+		t.Errorf("incorrect number of vertices. Expected %v, got %v", 8, len(m.Vertices))
+	}
+
+	expZ := []float64{ 3, 4, 1, 2 }
+	for i, v := range m.Vertices[:4] {
+		expX := float64(i % 2)
+		if v[0] != expX {
+			t.Errorf("incorrect X on vertex %v. Expected %v, got %v", i, expX, v[0])
+		}
+
+		expY := float64(i / 2)
+		if v[1] != expY {
+			t.Errorf("incorrect Y on vertex %v. Expected %v, got %v", i, expY, v[1])
+		}
+
+		if v[2] != expZ[i] {
+			t.Errorf("incorrect Z on vertex %v. Expected %v, got %v", i, expZ[i], v[2])
+		}
+	}
+
+	for i, v := range m.Vertices[4:] {
+		expX := float64(i % 2)
+		if v[0] != expX {
+			t.Errorf("incorrect X on vertex %v. Expected %v, got %v", i, expX, v[0])
+		}
+
+		expY := float64(i / 2)
+		if v[1] != expY {
+			t.Errorf("incorrect Y on vertex %v. Expected %v, got %v", i, expY, v[1])
+		}
+
+		expZ := float64(0)
+		if v[2] != expZ {
+			t.Errorf("incorrect Z on vertex %v. Expected %v, got %v", i, expZ, v[2])
+		}
+	}
+
+	expTris := [][3]uint{
+		// Top, clockwise
+		{ 0, 2, 3 },
+		{ 3, 1, 0 },
+		// Base, ccw
+		{ 4, 5, 7 },
+		{ 7, 6, 4 },
+		// South, clockwise
+		{ 4, 0, 1 },
+		{ 1, 5, 4 },
+		// North, clockwise
+		{ 7, 3, 2 },
+		{ 2, 6, 7 },
+		// East, clockwise
+		{ 5, 1, 3 },
+		{ 3, 7, 5 },
+		// West, clockwise
+		{ 6, 2, 0 },
+		{ 0, 4, 6 },
+	}
+
+	for i, tri := range m.Triangles {
+		if expTris[i][0] != tri[0] || expTris[i][1] != tri[1] || expTris[i][2] != tri[2] {
+			t.Errorf("incorrect triangle %v. Expected %v, got %v", i, expTris[i], tri)
+		}
+	}
+}
